1e0af8b112cb4d11cf37db3bb5b6421215700e2c
Partha Chakraborty <1305117.pc@ugrad.cse.buet.ac.bd>
2021-12-07 09:08:52 -0500

    evaluate updated

--- ace_jdt_job.sh
+++ ace_jdt_job.sh
@@ -2,9 +2,9 @@
 #SBATCH --account=def-m2nagapp
 #SBATCH --ntasks=1               # number of MPI processes
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=4G      # memory; default unit is megabytes
+#SBATCH --mem-per-cpu=8G      # memory; default unit is megabytes
 #SBATCH --time=18:20:00           # time (DD-HH:MM)
-#SBATCH --gres=gpu:p100:1
+#SBATCH --gres=gpu:v100l:1
 #SBATCH --output=%x-%j.out
 #SBATCH --mail-user=p9chakra@uwtaerloo.ca
 #SBATCH --mail-type=ALL
