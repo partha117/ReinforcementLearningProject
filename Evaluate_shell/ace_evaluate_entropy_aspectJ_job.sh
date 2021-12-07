641c4fbfc6d7b04860a7a3734ebe3ae0c75fdafc
Partha Chakraborty <1305117.pc@ugrad.cse.buet.ac.bd>
2021-12-07 18:50:37 -0500

    evaluate updated

--- Evaluate_shell/ace_evaluate_entropy_aspectJ_job.sh
+++ Evaluate_shell/ace_evaluate_entropy_aspectJ_job.sh
@@ -8,7 +8,7 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --mail-user=p9chakra@uwtaerloo.ca
 #SBATCH --mail-type=ALL
-export CUDA_LAUNCH_BLOCKING=1; python /home/partha9/CS885-RProject/Evaluate_AC.py \
+export CUDA_LAUNCH_BLOCKING=1; python /home/partha9/CS885-RProject/Evaluate_ACV2.py \
 --file_path /project/def-m2nagapp/partha9/LTR/ \
 --model_path Models/AC/Entropy/AspectJ/AspectJ_AC_Entropy_V2_policy_model_39.0.pt \
 --result_path /project/def-m2nagapp/partha9/LTR/Results/AC/Entropy/AspectJ/ \
