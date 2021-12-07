c3c18447867f9d2bec7b1609158c0816294e204a
Partha Chakraborty <1305117.pc@ugrad.cse.buet.ac.bd>
2021-12-07 08:56:20 -0500

    evaluate updated

--- ace_jdt_job.sh
+++ ace_jdt_job.sh
@@ -8,11 +8,9 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --mail-user=p9chakra@uwtaerloo.ca
 #SBATCH --mail-type=ALL
-export CUDA_LAUNCH_BLOCKING=1; python /home/partha9/CS885-RProject/AC_Entropy.py \
+export CUDA_LAUNCH_BLOCKING=1; python /home/partha9/CS885-RProject/AC_EntropyV2.py \
 --file_path /project/def-m2nagapp/partha9/LTR/ \
 --cache_path /scratch/partha9/.buffer_cache_ac_jdt \
---prev_policy_model_path /project/def-m2nagapp/partha9/LTR/Models/AC/Entropy/JDT/JDT_New_AC_Entropy_policy_model_99.0.pt \
---prev_value_model_path /project/def-m2nagapp/partha9/LTR/Models/AC/Entropy/JDT/JDT_New_AC_Entropy_value_model_99.0.pt \
 --train_data_path Data/TrainData/Bench_BLDS_JDT_Dataset.csv \
 --save_path /project/def-m2nagapp/partha9/LTR/Models/AC/Entropy/JDT/ \
 --project_name JDT
