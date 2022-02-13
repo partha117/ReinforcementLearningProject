9ae04de8ddc575703048e24c2cae90165f2b10be
Partha Chakraborty <1305117.pc@ugrad.cse.buet.ac.bd>
2022-02-13 12:47:51 -0500

    tomcat added

--- AC_EntropyV3.py
+++ AC_EntropyV3.py
@@ -267,6 +267,7 @@ if __name__ == "__main__":
     # start_from = 0
     # dev = "cuda:0"
     Path(file_path).mkdir(parents=True, exist_ok=True)
+    Path(save_path).mkdir(parents=True, exist_ok=True)
     env = LTREnvV5(data_path=file_path + train_data_path, model_path="microsoft/codebert-base",
                    tokenizer_path="microsoft/codebert-base", action_space_dim=31, report_count=None, code_max_len=2048,
                    report_max_len=512,
